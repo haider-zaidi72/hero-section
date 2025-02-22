62f9933a139449cd244ffe698a8fb7cf3ce002d8
urooj haider <suhz1100@gmail.com>
2025-02-22 12:21:10 +0500

    hersection

---
+++ scrip.js
@@ -0,0 +1,15 @@
+
+
+alert('Welcome to Dmitrii Rogoza');
+
+alert("Welcome to JS Land.\nAnd happy coding.");
+
+let password = prompt("Enter your password:");
+
+if (password !== "correctPassword") {
+    alert("Wrong password! Please try again.");
+} else {
+    alert("Access granted!");
+}
+alert("happy coding.\nAnd Prevent this page from creating additional dialongs");
+alert("Hello! I can run JS through console in my webpage");
